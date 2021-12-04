074febc49ebbea679b3c38d9541160cb5b20581f
Kelpsy <92172180+Kelpsy@users.noreply.github.com>
2021-12-04 17:38:23 +0100

    Day 4

---
+++ day4.zig
@@ -0,0 +1,163 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+fn readLine(lineBuffer: *ArrayList(u8), reader: std.fs.File.Reader) !?[]u8 {
+    try lineBuffer.resize(0);
+    reader.readUntilDelimiterArrayList(lineBuffer, '\n', std.math.inf_u64) catch |e| switch (e) {
+        error.EndOfStream => return null,
+        else => return e,
+    };
+    return lineBuffer.items;
+}
+
+const BoardNumber = struct {
+    number: u8,
+    called: bool,
+};
+
+const Board = struct { numbers: [5][5]BoardNumber };
+
+fn checkBoard(board: *const Board) bool {
+    for (board.numbers) |row| {
+        if (for (row) |number| {
+            if (!number.called) {
+                break false;
+            }
+        } else true) {
+            return true;
+        }
+    }
+
+    var col_index: usize = 0;
+    return while (col_index < 5) : ({
+        col_index += 1;
+    }) {
+        if (for (board.numbers) |row| {
+            if (!row[col_index].called) {
+                break false;
+            }
+        } else true) {
+            break true;
+        }
+    } else false;
+}
+
+fn parse(numbers: *ArrayList(u8), boards: *ArrayList(Board), lineBuffer: *ArrayList(u8), reader: std.fs.File.Reader) !void {
+    const numbers_line = (try readLine(lineBuffer, reader)) orelse unreachable;
+    {
+        var iter = std.mem.split(numbers_line, ",");
+        while (true) {
+            try numbers.append(try std.fmt.parseUnsigned(u8, iter.next() orelse break, 10));
+        }
+    }
+
+    outer: while (true) {
+        _ = try readLine(lineBuffer, reader);
+        var board = Board{ .numbers = std.mem.zeroes([5][5]BoardNumber) };
+        var i: usize = 0;
+        while (i < 5) : ({
+            i += 1;
+        }) {
+            const line = (try readLine(lineBuffer, reader)) orelse break :outer;
+            var iter = std.mem.split(line, " ");
+            var j: usize = 0;
+            while (j < 5) {
+                const str = iter.next() orelse break;
+                if (str.len == 0) {
+                    continue;
+                }
+                board.numbers[i][j] = .{ .number = try std.fmt.parseUnsigned(u8, str, 10), .called = false };
+                j += 1;
+            }
+        }
+        try boards.append(board);
+    }
+}
+
+fn part1(allocator: *Allocator, lineBuffer: *ArrayList(u8), reader: std.fs.File.Reader) !u32 {
+    var numbers = ArrayList(u8).init(allocator);
+    var boards = ArrayList(Board).init(allocator);
+    try parse(&numbers, &boards, lineBuffer, reader);
+
+    for (numbers.items) |number| {
+        for (boards.items) |*board| {
+            for (board.numbers) |*row| {
+                for (row) |*board_number| {
+                    if (board_number.number == number) {
+                        board_number.called = true;
+                    }
+                }
+            }
+
+            if (checkBoard(board)) {
+                var sum: u32 = 0;
+                for (board.numbers) |row| {
+                    for (row) |board_number| {
+                        if (!board_number.called) {
+                            sum += board_number.number;
+                        }
+                    }
+                }
+                return sum * number;
+            }
+        }
+    }
+
+    unreachable;
+}
+
+fn part2(allocator: *Allocator, lineBuffer: *ArrayList(u8), reader: std.fs.File.Reader) !u32 {
+    var numbers = ArrayList(u8).init(allocator);
+    var boards = ArrayList(Board).init(allocator);
+    var last_product: ?u32 = null;
+    try parse(&numbers, &boards, lineBuffer, reader);
+
+    for (numbers.items) |number| {
+        var i: usize = 0;
+        while (i < boards.items.len) {
+            const board = &boards.items[i];
+            for (board.numbers) |*row| {
+                for (row) |*board_number| {
+                    if (board_number.number == number) {
+                        board_number.called = true;
+                    }
+                }
+            }
+
+            if (checkBoard(board)) {
+                var sum: u32 = 0;
+                for (board.numbers) |row| {
+                    for (row) |board_number| {
+                        if (!board_number.called) {
+                            sum += board_number.number;
+                        }
+                    }
+                }
+                last_product = sum * number;
+                _ = boards.swapRemove(i);
+            } else {
+                i += 1;
+            }
+        }
+    }
+
+    return last_product.?;
+}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    var args = std.process.args();
+    _ = args.skip();
+    const part = try std.fmt.parseUnsigned(u8, try args.next(allocator).?, 10);
+    const path = try args.next(allocator).?;
+    const inputFile = try std.fs.cwd().openFile(path, .{ .read = true });
+    defer inputFile.close();
+    var lineBuffer = ArrayList(u8).init(allocator);
+    const product = switch (part) {
+        1 => try part1(allocator, &lineBuffer, inputFile.reader()),
+        2 => try part2(allocator, &lineBuffer, inputFile.reader()),
+        else => unreachable,
+    };
+    std.debug.print("Product: {}", .{product});
+}
